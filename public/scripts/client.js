873eca392b4cf54c3afd69ea1fa04de2a80e5747
vagrant <Dcameroncheng@gmail.com>
2020-04-22 19:35:31 +0000

    separate tweets from combining with one another

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,32 +4,57 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
+ 
   const createTweetElement = function(tweetObj) {
-  const $tweet = $(`<article class="article-tweet">
-            <header class="header-tweetbox">
+    const $tweet = $(` <article class="article-tweet">
+            <header>
               <img src="${tweetObj.user.avatars}">
               <span class="header-tweet-name">${tweetObj.user.name}</span>
-              <a href="#"><p class="header-tweet-handle"${tweetObj.user.handle}c</p></a>
+              <a href="#"> <p class="header-tweet-handle">${tweetObj.user.handle}</p> </a>
             </header>
-              <span class="tweet-content">${tweetObj.content.text} </span>
-              <span class="span-blackline"></span>
+            <span class="tweet-content">${tweetObj.content.text} </span>
+            <span class="span-blackline"></span>
             <footer class="footer-tweets">  
               <span class="footer-tweet-date">${tweetObj.created_at}</span>
               <span class="footer-icons">⚑ ↱↲ ♥︎</span>
             </footer>
-          </article>`)
-        $(".article-tweet").append($tweet);
-    //return $tweet
-  }
-  createTweetElement({
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
+            </article>
+          `);
+    return $tweet;
+  };
+
+  const renderTweets = function(tweetsArray) {
+    for (const tweetObj of tweetsArray) {
+      let tweet = createTweetElement(tweetObj);
+      $("#tweets-container").append(tweet);
+    }
+  };
+
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
         "handle": "@SirIsaac"
       },
-    "content": {
+      "content": {
         "text": "If I have seen further it is by standing on the shoulders of giants"
       },
-    "created_at": 1461116232227
-  })
-})  
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+  renderTweets(data);
+});
+
